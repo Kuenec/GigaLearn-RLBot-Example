8fc63d7a277c7115a916b2fa09d199348b6b15bf
Kuenec <167933777+Kuenec@users.noreply.github.com>
2025-08-24 16:25:42 -0500

    Add files via upload

---
+++ RLBotClient.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <rlbot/bot.h>
+#include <RLGymCPP/ObsBuilders/ObsBuilder.h>
+#include <RLGymCPP/ActionParsers/ActionParser.h>
+#include <GigaLearnCPP/Util/InferUnit.h>
+#include <GigaLearnCPP/Util/ModelConfig.h> 
+
+#include <filesystem> 
+
+struct RLBotParams {
+    
+    int port;
+    int tickSkip;
+    int actionDelay;
+    bool deterministic = false;
+    bool useGPU = true;
+
+    RLGC::ObsBuilder* obsBuilder = nullptr;
+    RLGC::ActionParser* actionParser = nullptr;
+    GGL::InferUnit* inferUnit = nullptr;
+
+    int obsSize;
+    GGL::PartialModelConfig policyConfig;
+    GGL::PartialModelConfig sharedHeadConfig;
+};
+
+class RLBotBot : public rlbot::Bot {
+public:
+    
+    RLBotParams params;
+
+    RLGC::Action
+        action = {},
+        controls = {};
+
+    bool updateAction = true;
+    float prevTime = 0;
+    int ticks = -1;
+
+    RLBotBot(int _index, int _team, std::string _name, const RLBotParams& params);
+    ~RLBotBot();
+
+    rlbot::Controller GetOutput(rlbot::GameTickPacket gameTickPacket) override;
+};
+
+namespace RLBotClient {
+    void Run(const RLBotParams& params);
+}
