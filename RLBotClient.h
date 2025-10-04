8d0233bef2938f3a81958ec228bb3bd7af0e8417
Kuenec <167933777+Kuenec@users.noreply.github.com>
2025-10-04 09:18:29 -0500

    Update RLBotClient.h

--- RLBotClient.h
+++ RLBotClient.h
@@ -10,6 +10,36 @@
 #include <memory>
 #include <map>
 
+namespace RLConstructor {
+    constexpr float JUMP_MAX_TIME = 0.2f;
+    constexpr float JUMP_MIN_TIME = 0.025f;
+    constexpr float FLIP_TORQUE_TIME = 0.65f;
+    constexpr float FLIP_TORQUE_MIN_TIME = 0.41f;
+    constexpr float FLIP_PITCHLOCK_TIME = 1.0f;
+    constexpr float BOOST_MIN_TIME = 0.1f;
+    constexpr float POWERSLIDE_RISE_RATE = 5.f;
+    constexpr float POWERSLIDE_FALL_RATE = 2.f;
+    constexpr float SUPERSONIC_MAINTAIN_MAX_TIME = 1.f;
+    constexpr float CAR_AUTOFLIP_TIME = 0.4f;
+    constexpr float CAR_AUTOFLIP_ROLL_THRESH = 2.8f;
+    constexpr float CAR_AUTOFLIP_NORMZ_THRESH = 0.70710678118f;
+    constexpr float BUMP_COOLDOWN_TIME = 0.25f;
+}
+
+namespace RLConst {
+    constexpr float GRAVITY_Z = -650.f;
+    constexpr float AIR_DRAG = 0.97f;
+    constexpr float CAR_MAX_SPEED = 2300.f;
+    constexpr float CAR_MAX_THROTTLE_SPEED = 1410.f;
+    constexpr float BOOST_ACCEL = 991.666f;
+    constexpr float BOOST_CONSUMPTION_RATE = 33.3f;
+    constexpr float SUPERSONIC_THRESHOLD = 2200.f;
+    constexpr float DEMO_SPEED_THRESHOLD = 2300.f;
+    constexpr float BALL_RADIUS = 91.25f;
+    constexpr float BALL_MAX_SPEED = 6000.f;
+}
+
+
 struct RLBotParams {
     int port;
     int tickSkip;
@@ -54,6 +84,9 @@ public:
 
         bool hasFlipped = false;
         float airTime = 0;
+
+        float autoFlipTimer = 0.f;
+        bool autoFlipAttacking = false;
     };
     std::map<int, PlayerInternalState> internalPlayerStates;
     int lastTeamScores[2] = {0, 0};
